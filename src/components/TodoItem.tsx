f4e5d53d1e6681d7935a4910904fd103570fa481
yamamotch <yamamoto.cyber610@gmail.com>
2024-11-13 22:48:54 +0900

    Edit TodoItem .tsx

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListItem, ListItemText, CardContent, IconButton } from '@mui/material';
+import { ListItem, ListItemText, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 interface TodoItemProps {
